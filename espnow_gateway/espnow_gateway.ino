2df53309d6e08b82d65efe43d305e6288233984f
Jonas Byström <65979819+jonasbystrom@users.noreply.github.com>
2022-01-01 18:08:31 +0100

    Add files via upload

--- espnow_gateway/espnow_gateway.ino
+++ espnow_gateway/espnow_gateway.ino
@@ -1,23 +1,26 @@
-#define SKETCH_NAME    "ESP-NOW GATEWAY 8266"
-#define SKETCH_VERSION "2021-12-30"
-#define SKETCH_ABOUT   "ESP8266 ESP-Now Gateway template code and demonstrator of simultaneous ESP-Now and WiFi."
+#define SKETCH_NAME    "ESP-NOW GATEWAY"
+#define SKETCH_VERSION "2022-01-01"
+#define SKETCH_ABOUT   "ESP-Now Gateway template code and demonstrator of simultaneous ESP-Now and WiFi."
 
 /*
- * This is a template code for an ESP-Now Gateway for ESP8266. It assumes Arduino IDE.
- * (A later version will also include the small differencies needed for an ESP32.)
+ * This is a template code for an ESP-Now Gateway for ESP8266 and ESP32. It assumes Arduino IDE.
  * 
+ * Note. ESP8266 and ESP32 use different WiFi implementations why you must set the appropriate
+ * #define USE_ESP8266 or USE_ESP32. Please remember to also set the appropriate BOARD in the 
+ * ARDUINO IDE to find the correct include files.
+ *
  * The Gateway receives messages on the Espressif proprietary protocol ESP-Now. It also provides a simple WEB server
  * displaying the received temperature data from all sensors. This is to demonstrate a single ESP8266 is capable of 
  * communicating on ESP-Now and 2.4 GHz Wifi simoultaneously. One restriction apply, this only works on WiFi channel 1 
  * and you must therefore set the router to "fixed" channel 1.
  * 
  * In order to perform the demonstration you will need: 
- * - 1x ESP8266 programmed with this sketch, acting as Gateway (to the WiFi/internet), and 
- * - 1-20x ESP8266 programmed with the espnow_sensor sketch and equipped with a temperature sensor (or anything else
- * you connect and modify the code for).
+ * - 1x ESP8266 or ESP32 programmed with this sketch, acting as Gateway (to the WiFi/internet), and 
+ * - 1-20x ESP8266 or ESP32 programmed with the espnow_sensor sketch and equipped with a temperature sensor 
+ * (or anything else you connect and modify the code for).
  * 
- * This is a simple demo sketch but fully working. It demostrates simultaneous WiFi and eSP-Now on a ESP 8266.
- * You can add other Internet services like MQTT, Blynk, ThingSpeak etc. (I have been running this for 1-2 years with 
+ * This is a simple demo sketch but fully working. It demostrates simultaneous WiFi and ESP-Now on ESP8266 and ESP32.
+ * You can add other Internet services like MQTT, Blynk, ThingSpeak etc. (I have been running this for 1.5+ years with 
  * those services.) 
  * 
  * CREDITS:
@@ -30,17 +33,43 @@
  */
 
 //  HARDWARE
-//  - D1 Mini Pro V2.0.0 (But any ESP8266 board should work)
-//
+//  - WEMOS/LOLIN D1 Mini Pro V2.0.0  (but any ESP8266 board should work)
+//  or
+//  - WEMOS/LOLIN D32 Pro  (but any ESP32 board should work)
+// 
 //  HISTORY:
-//  2020-10-27  First public version
+//  2021-12-30  First public version
+//  2022-01-01  Support for ESP32 added. 
 //
 
-
+// ------------------------------------------------------------------------------------------
+// ESP CONFIGS
+// ------------------------------------------------------------------------------------------
+//
+// IMPORTANT. Select ESP8266 or ESP32. - Remember to also set the appropriate ESP8266 or ESP32 board in the ARDUINO IDE !
+#define USE_ESP8266                 // Select (uncomment) one of ESP8266 or ESP32. (And set BOARD in ARDUINO IDE.) 
+//#define USE_ESP32
+      
+// Different Wifi and ESP-Now implementations for ESP8266 and ESP32
+#ifdef USE_ESP8266
+#include <espnow.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266WebServer.h>
-#include <espnow.h>
+#endif
+
+#ifdef USE_ESP32
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WebServer.h>
+
+#ifndef LED_BUILTIN 
+#define LED_BUILTIN  5             // Set this to the built-in LED number of your ESP32 board
+#endif
+
+#endif
 
 
 // ------------------------------------------------------------------------------------------
@@ -83,7 +112,6 @@ typedef struct sensor_data_t {
 // Router WiFi Credentials (runnning on 2.4GHz and Channel=1)
 #define SOFTAP_SSID       "...YOUR SSID Network Name..."
 #define SOFTAP_PASS       "...YOUR Network Password ..."
-#define CHANNEL           1
 
 #define UNITS             20          // No of esp-now sensor units supported to receive from. ESP-Now has a maximum of 20
 
@@ -91,7 +119,12 @@ typedef struct sensor_data_t {
 // ------------------------------------------------------------------------------------------
 // GLOBALS
 // ------------------------------------------------------------------------------------------
+#ifdef USE_ESP8266
 ESP8266WiFiMulti wifiMulti;
+#endif
+#ifdef USE_ESP32
+WiFiMulti wifiMulti;
+#endif
 
 sensor_data_t bufSensorData;          // buffer for incoming data
 sensor_data_t sensorData[UNITS+1];    // buffer for all sensor data
@@ -102,14 +135,32 @@ sensor_data_t sensorData[UNITS+1];    // buffer for all sensor data
 // ------------------------------------------------------------------------------------------
 //
 // This is a callback function from ESP (?), anyway (!IMPORTANT)
+//
+//   (Note. I noted now, 1 1/2 years later, when i clean this up and retest a gateway with ESP32 - this initVariant
+//   seems not to be called by ESP routines anymore. I have as a fix called this routine manually in the setup.
+//   This could of course (?) be done also for ESP8266. But i dont want to touch that code since it has been working 
+//   for 1.5 years now.)
+//
 void initVariant()
 {
-  wifi_set_macaddr(SOFTAP_IF, &GatewayMac[0]);       //8266 code
-  wifi_set_macaddr(STATION_IF, &GatewayMac[0]);      //8266 code
+  #ifdef USE_ESP8266
+  wifi_set_macaddr(SOFTAP_IF, &GatewayMac[0]);          //8266 code
+  wifi_set_macaddr(STATION_IF, &GatewayMac[0]);         //8266 code
+  #endif
+
+  #ifdef USE_ESP32
+  WiFi.mode(WIFI_AP); 
+  esp_wifi_set_mac(ESP_IF_WIFI_AP, &GatewayMac[0]);     //ESP32 code
+  #endif
 }
 
 // Callback when data is received from any Sender
+#ifdef USE_ESP8266
 void OnDataRecv(uint8_t *mac_addr, uint8_t *data, uint8_t data_len)
+#endif
+#ifdef USE_ESP32
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
+#endif
 {
   digitalWrite (LED_BUILTIN, !HIGH);                // Led ON
   
@@ -153,7 +204,12 @@ void OnDataRecv(uint8_t *mac_addr, uint8_t *data, uint8_t data_len)
 // ------------------------------------------------------------------------------------
 // WEB server functions
 // ------------------------------------------------------------------------------------
+#ifdef USE_ESP8266
 ESP8266WebServer server(80);
+#endif
+#ifdef USE_ESP32
+WebServer server(80);
+#endif
 
 void handleRoot() {
   digitalWrite (LED_BUILTIN, !HIGH);                    // Led ON
@@ -191,7 +247,7 @@ void setup()
 // ------------------------------------------------------------------------------------
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite (LED_BUILTIN, !HIGH);              // Led ON
+  digitalWrite (LED_BUILTIN, !HIGH);              // Led ON    (Most often not working on ESP32)
 
   // Init Serial
   Serial.begin(115200);
@@ -207,12 +263,20 @@ void setup()
   Serial.println(SKETCH_ABOUT);
   Serial.println("===========================================");
 
+  #ifdef USE_ESP32
+  // initVariant() seems (?) not to be called anymore for ESP32. It used to ...? Anyway, just call it from here.
+  // (And of course, we could maybe just move all the code here, also for ESP8266. Some other day ...) 
+  initVariant();  
+  #endif
+  
   // Connect to WiFi ------------------------------
   Serial.print("Connecting to WiFi ");
 
+  #ifdef USE_ESP8266
   WiFi.disconnect();                              // clear all configs
   WiFi.softAPdisconnect();
-
+  #endif
+  
   // Set device in AP mode to begin with
   WiFi.mode(WIFI_AP_STA);                         // AP _and_ STA is required (!IMPORTANT)
 
@@ -224,19 +288,19 @@ void setup()
 
   // Come here - we are connected
   Serial.println(" Done");
-
+ 
   // Print WiFi data
   Serial.println("Set as AP_STA station.");
   Serial.print  ("SSID: "); Serial.println(WiFi.SSID());
   Serial.print  ("Channel: "); Serial.println(WiFi.channel());
-  Serial.print("IP address: "); Serial.println(WiFi.localIP());
+  Serial.print  ("IP address: "); Serial.println(WiFi.localIP());
   delay(1000);
 
 
   // Initialize ESP-Now ---------------------------
 
-  // set SSID and channel 
-  int channel = CHANNEL;
+  // Config gateway AP - set SSID and channel 
+  int channel = WiFi.channel();
   if (WiFi.softAP(SOFTAP_SSID, SOFTAP_PASS, channel, 1)) {
     Serial.println("AP Config Success. AP SSID: " + String(SOFTAP_SSID));
   } else {
@@ -248,7 +312,14 @@ void setup()
   Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
 
   // Init ESP-Now 
-  if (esp_now_init() == 0) {
+  #ifdef USE_ESP8266
+  #define ESPNOW_SUCCESS 0
+  #endif
+  #ifdef USE_ESP32
+  #define ESPNOW_SUCCESS ESP_OK
+  #endif  
+
+  if (esp_now_init() == ESPNOW_SUCCESS) {
     Serial.println("ESP - Now Init Success");
   } else {
     Serial.println("ESP - Now Init Failed");
